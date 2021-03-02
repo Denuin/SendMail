2584f18e73333c1e9c3eb02d76e93329694c33b3
Denuin <denuin@live.com>
2021-03-03 01:05:23 +0800

    Init Commit

---
+++ src/SendMail/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SendMail
+{
+    internal class Program
+    {
+        private static readonly Dictionary<string, string> argsMap = new Dictionary<string, string>();
+
+        /// <summary>
+        /// Main
+        /// </summary>
+        /// <param name="args">-user "邮件帐号/密码" -smtp "smtp.office365.com:587" -to "1@abc.com;2@abc.com" -from "123@abc.com" -subject "Test Email" -body "The test is working!" [-att "D:\1.txt;D:\2.txt"] </param>
+        private static void Main(string[] args)
+        {
+            if (args != null)
+            {
+                int argsLength = args.Length;
+
+                for (int i = 0; i < argsLength; i++)
+                {
+                    if (args[i][0] == '-')
+                    {
+                        string key = args[i].ToLower();
+                        string value = args[i + 1];
+                        argsMap[key] = value.Trim();
+                    }
+                }
+            }
+
+            if (CheckArgsMap(argsMap))
+            {
+                Sender sender = new Sender();
+                sender.Send(argsMap).Wait();
+            }
+        }
+
+        private static bool CheckArgsMap(Dictionary<string, string> dic)
+        {
+            bool result = true;
+            HashSet<string> hsCheck = new HashSet<string>
+            {
+                "-to",
+                "-from",
+                "-subject",
+                "-body",
+                "-smtp",
+                "-user"
+            };
+
+            HashSet<string> hsKey = new HashSet<string>(dic.Keys);
+            hsCheck.ExceptWith(hsKey);
+            foreach (var p in hsCheck)
+            {
+                Console.WriteLine($"Error: 缺少参数 {p} !");
+                result = false;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/SendMail/Sender.cs
@@ -0,0 +1,99 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace SendMail
+{
+    internal class Sender
+    {
+        private readonly ISmtpClient _smtp;
+
+        public Sender()
+        {
+            _smtp = new MailKit.Net.Smtp.SmtpClient();
+        }
+
+        public async Task<bool> Send(Dictionary<string, string> argsMap)
+        {
+            bool result = false;
+
+            string[] to = argsMap["-to"].Split(';');
+            string[] from = argsMap["-from"].Split(',');
+            string subject = argsMap["-subject"];
+            string body = argsMap["-body"];
+            string[] smtp = argsMap["-smtp"].Split(':');
+            string[] user = argsMap["-user"].Split('/');
+
+            string[] att = null;
+            if (argsMap.TryGetValue("-att", out string valueAtt))
+            {
+                att = valueAtt.Split(';');
+            }
+
+            if (smtp?.Length != 2)
+            {
+                Console.WriteLine("Send Error: -smtp格式有误！");
+                return result;
+            }
+            if (user?.Length != 2)
+            {
+                Console.WriteLine("Send Error: -user格式有误！");
+                return result;
+            }
+
+            #region 发邮件
+
+            _smtp.ConnectAsync(smtp[0], Convert.ToInt32(smtp[1]), SecureSocketOptions.StartTls).Wait();
+            _smtp.AuthenticateAsync(user[0], user[1]).Wait();
+
+            _smtp.MessageSent += (sender, args) =>
+            {
+                Console.WriteLine($"Send Message: {args.Response}");
+            };
+
+            var message = new MimeMessage();
+            InternetAddressList list = new InternetAddressList();
+            foreach (var p in to)
+            {
+                list.Add(new MailboxAddress(p, p));
+            }
+            message.To.AddRange(list);
+            if (from.Length > 1)
+            {
+                message.From.Add(new MailboxAddress(from[0], from[1]));
+            }
+            else
+            {
+                message.From.Add(new MailboxAddress(from[0], from[0]));
+            }
+            message.Subject = subject;
+
+            var builder = new BodyBuilder();
+            if (att != null)
+            {
+                foreach (var p in att)
+                {
+                    if (File.Exists(p))
+                    {
+                        builder.Attachments.Add(p);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"提示: 附件文件 {p} 不存在。");
+                    }
+                }
+            }
+            builder.TextBody = body;
+            message.Body = builder.ToMessageBody();
+            await _smtp.SendAsync(message);
+
+            #endregion 发邮件
+
+            return result;
+        }
+    }
+}
